4041394257e5e8da401d9743c7edf6c2ff98b678
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2025-02-26 21:09:58 -0600

    Se agregan las validaciones, la encriptacion de contraseñas y de toke

---
+++ utils/db.validators.js
@@ -0,0 +1,37 @@
+//Validar datos relacionados BD
+
+import { isValidObjectId } from "mongoose"
+import User from '../src/User/user.model.js'
+
+export const existUsername = async(username, user) =>{
+    const alreadyUsername = await User.findOne({username})
+    if(alreadyUsername && alreadyUsername._id != user.uid){
+        console.error(`Username ${username} is already taken`)
+        throw new Error(`Username ${username} is already taken`)
+    }
+}
+export const existEmail = async(email, user) => {
+    const alreadyEmail = await User.findOne({email})
+    if(alreadyEmail && alreadyEmail._id != user.uid){
+        console.error(`Email ${email} is already taken`)
+        throw new Error(`Email ${email} is already taken`)
+    }
+}
+
+//Validar que sea un id
+export const objectIdValid = (objectId)=>{
+    if(!isValidObjectId(objectId)) {
+        throw new Error(`Keeper is not a valid ObjectId`)
+    }
+}
+
+export const findUser = async(id)=>{
+    try{
+        const userExist = await User.findById(id)
+        if(!userExist) return false
+        return userExist
+    }catch(err){
+        console.error(err)
+        return false
+    }
+}
